3954ae24f1dfbf2ae8911f9a5c705924b8f4c84c
Oliver <oliverbarnum32@gmail.com>
2025-02-18 23:43:16 -0600

    Sigh

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,10 @@
-import { ensureServerEnvironment } from './browserCheck.js';
+function ensureServerEnvironment(functionName) {
+  if (typeof window !== 'undefined') {
+    throw new Error(
+      `${functionName} can only be used in a server-side context (getStaticProps, getServerSideProps, API routes). See documentation for proper usage.`
+    );
+  }
+} 
 
 export {
   getUserFromToken,
